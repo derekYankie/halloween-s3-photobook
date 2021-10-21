b5aede1c50806f1c73270ec206971aff8cc02dc3
Derek Afriyie <derekafriyie@Dereks-MBP19.lan>
2021-10-21 11:38:33 -0400

    created webpage template

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-app = Flask (__name__)
+app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "Happy Haloween"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
