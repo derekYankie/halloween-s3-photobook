c07ab45ae894eae6298cf290feea82827b432023
Derek Afriyie <derek.afriyie@gmail.com>
2021-10-23 22:07:48 -0400

    Halloween files page created.

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+import boto3
+from filters import datetimeformat, file_type
 
+#Use boto3 to access s3 resource
+#I'm having boto3 use my aws cli creds insted of storing it in an enviorment variable file
+boto3.setup_default_session(profile_name='default')
+s3 = boto3.resource('s3')
+
+
+#create app
 app = Flask(__name__)
 Bootstrap(app)
+#register jinja filters
+app.jinja_env.filters['datetimeformat'] = datetimeformat
+app.jinja_env.filters['file_type'] = file_type
 
+#make index page
 @app.route('/')
 def index():
+    print("Dude, you're webpage is running!")
     return render_template('index.html')
+
+#list files in flask-s3-photobook bucket
+@app.route('/files')
+def files():
+ 
+    my_bucket = s3.Bucket('halloween-s3-photobook-2021')
+    #print all items in s3 Bucket
+    for file in my_bucket.objects.all():
+        print(f"file name: {file.key}")
+    summaries = my_bucket.objects.all()
+    print(f"S3 Bucket Object:", my_bucket)
+
+    return render_template('files.html', my_bucket=my_bucket, files=summaries)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ filters.py
@@ -0,0 +1,16 @@
+import os
+import mimetypes
+import arrow
+
+#Use jinja datetimeformat
+def datetimeformat(date_str):
+    dt = arrow.get(date_str)
+    return dt.humanize()
+
+def file_type(key):
+    file_info = os.path.splitext(key)
+    file_extension = file_info[1]
+    try:
+        return mimetypes.types_map[file_extension]
+    except KeyError:
+        return 'Unknown'
